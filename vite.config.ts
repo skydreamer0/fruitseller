f203665cf807c5b0a34b413c65a78cb76cd431c7
George <skyaski00@gmail.com>
2025-02-03 22:25:20 +0800

    fix some issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? '/販售水果/' : '/',  // 在GitHub Pages上的base URL
+  base: '/fruitseller/',  // 直接設定 GitHub Pages 的 base URL
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
